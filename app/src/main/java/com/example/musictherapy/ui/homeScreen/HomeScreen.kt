4dffa12caea4ac8a2b523c305eba311ae6f580d3
Bhargav200004 <Bhargav200004@gmail.com>
2024-01-09 17:12:30 +0530

    adding homeScreen bottom bar

--- app/src/main/java/com/example/musictherapy/ui/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/musictherapy/ui/homeScreen/HomeScreen.kt
@@ -1,8 +1,103 @@
 package com.example.musictherapy.ui.homeScreen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material.icons.outlined.MusicNote
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.musictherapy.R
 
 @Composable
 fun HomeScreen() {
+    Scaffold(
+        topBar = {},
+        bottomBar = { BottomHomeScreenBar() }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues = paddingValues)
+        ) {
 
+        }
+    }
+}
+
+@Composable
+fun BottomHomeScreenBar() {
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 20.dp)
+            .shadow(
+                ambientColor = Color.White,
+                spotColor = Color.White,
+                elevation = 8.dp,
+                shape = RoundedCornerShape(
+                    topStart = 40.dp,
+                    topEnd = 40.dp
+                )
+            )
+            .clip(shape = RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp)),
+        contentAlignment = Alignment.BottomStart,
+    ) {
+        BottomAppBar(
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(height = 85.dp, width = 1000.dp),
+            actions = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 20.dp, bottom = 36.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                ) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            modifier = Modifier
+                                .size(30.dp),
+                            imageVector = Icons.Outlined.Home,
+                            contentDescription = "Home"
+                        )
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Outlined.MusicNote,
+                            contentDescription = "Music",
+                        )
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.heart),
+                            contentDescription = "Heart"
+                        )
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Outlined.LocationOn,
+                            contentDescription = "Location"
+                        )
+                    }
+                }
+            },
+        )
+    }
 }
