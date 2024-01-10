f3f0dedd2c2b9186f60baae3b5c41ca0028a6288
Bhargav200004 <Bhargav200004@gmail.com>
2024-01-10 15:58:27 +0530

    adding top bar in home screen

--- app/src/main/java/com/example/musictherapy/ui/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/musictherapy/ui/homeScreen/HomeScreen.kt
@@ -1,35 +1,49 @@
 package com.example.musictherapy.ui.homeScreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.LocationOn
 import androidx.compose.material.icons.outlined.MusicNote
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.musictherapy.R
 
 @Composable
 fun HomeScreen() {
     Scaffold(
-        topBar = {},
+        modifier = Modifier
+            .padding(horizontal = 20.dp),
+        topBar = { TopHomeScreenBar() },
         bottomBar = { BottomHomeScreenBar() }
     ) { paddingValues ->
         Column(
@@ -41,11 +55,61 @@ fun HomeScreen() {
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopHomeScreenBar() {
+    TopAppBar(
+        title = { Text(text = "") },
+        actions = { 
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                Row (){
+                    Box(
+                        modifier = Modifier
+                            .size(40.dp)
+                            .clip(shape = RoundedCornerShape(30.dp))
+                            .background(Color.Red)
+                            .padding(4.dp)
+                    ){
+                        Image(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            painter = painterResource(id = R.drawable.baseline_person_3_24),
+                            contentDescription = "Person"
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Column {
+                        Text(
+                            text = "Gujjla Bhargav Sai Durga",
+                            style = TextStyle(
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 18.sp
+                            )
+                        )
+                        Text(text = "Gold Membership")
+                    }
+                }
+
+                Icon(
+                    painter = painterResource(id = R.drawable.bell),
+                    contentDescription = "Bell icon",
+                )
+            }
+        }
+    )
+}
+
 @Composable
 fun BottomHomeScreenBar() {
     Box(
         modifier = Modifier
-            .padding(horizontal = 20.dp)
             .shadow(
                 ambientColor = Color.White,
                 spotColor = Color.White,
